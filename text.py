c10a97ad612bbd38dd5247bcae9414e1a9adfe75
shifasherin7078 <shiff553@gmail.com>
2025-11-18 13:39:00 +0530

    text.py

---
+++ text.py
@@ -0,0 +1,9 @@
+text=input("enter text:")
+words=text.split()
+word_count={}
+for w in words:
+    if w in word_count:
+        word_count[w]+=1
+    else:
+        word_count[w]=1
+print("word occuerence:",word_count)            
