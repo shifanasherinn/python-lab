aad306ceb9b9fba690022510818ac00681a55778
shifanasherinn <shiff553@gmail.com>
2026-01-01 20:35:19 +0530

    Delete copyoddlines.py

--- copyoddlines.py
+++
@@ -1,9 +0,0 @@
-
-source = "input.txt"
-destination = "output.txt"
-
-with open(source, "r") as f1, open(destination, "w") as f2:
-    lines = f1.readlines()
-    for i in range(len(lines)):
-        if (i + 1) % 2 != 0:
-            f2.write(lines[i])
