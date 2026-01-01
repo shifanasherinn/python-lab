6fe8410b2bf561bdbe21b02b1a2045ff45532013
shifanasherinn <shiff553@gmail.com>
2026-01-01 20:33:18 +0530

    Delete bggst3.py

--- bggst3.py
+++
@@ -1,9 +0,0 @@
-a=float(input("enter first number:"))
-b=float(input("enter second number:"))
-c=float(input("enter third number:"))
-if a>b and a>c:
-    print("the biggest number is:",a)
-elif b>a and b>c:
-    print("the biggest number is:",b)
-else:
-    print("the biggest number is:",c)
