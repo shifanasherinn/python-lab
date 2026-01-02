0d697677aa3af661319ee18b284ae3f168a23d9f
shifanasherinn <shiff553@gmail.com>
2026-01-02 22:49:43 +0530

    Update program16.py

--- program16.py
+++ program16.py
@@ -1,15 +1,7 @@
-# Program to count character frequency
 
-text = input("Enter a string: ")
 
-char_count = {}
-
-for char in text:
-    if char in char_count:
-        char_count[char] += 1
-    else:
-        char_count[char] = 1
-
-print("Character frequency:")
-for char, count in char_count.items():
-    print(f"'{char}': {count}")
+s= input("Enter a string: ")
+freq = {}
+for ch in s:
+    freq[ch] = freq.get(ch, 0) + 1
+print(freq)
